8d4785a2b76fdd92f3988ad4bbfbae2806589d7e
Jon <ruisong20@gmail.com>
2023-05-10 16:38:46 +0800

    [update] change package manager to lazy.nvim, add ftplugins for tex and text files

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -7,9 +7,15 @@ end
 indent.setup({
 	enabled = true,
 	colored_indent_levels = false,
-	space_char_blankline = " ", -- blank line placeholder
+
+	char = "▏",
+	context_char = "▏",
+	show_trailing_blankline_indent = false,
+	use_treesitter = true,
 	show_current_context = true,
 	show_current_context_start = true,
+
+	space_char_blankline = " ", -- blank line placeholder
 
 	-- excluded filetypes for lines
 	filetype_exclude = {
@@ -25,5 +31,4 @@ indent.setup({
 		"toggleterm",
 		"text",
 	},
-	char = "▏",
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,227 +1,212 @@
-local path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local packer_bootstrap
-
--- automatically detect and install Packer.nvim
-if vim.fn.empty(vim.fn.glob(path)) > 0 then
-	vim.notify("[Plugins] Packer.nvim installing, please wait ...")
-	packer_bootstrap = vim.fn.system({
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazy_path) then
+	vim.fn.system({
 		"git",
 		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		path,
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazy_path,
 	})
-	local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
-	if not string.find(vim.o.runtimepath, rtp_addition) then
-		vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
-	end
-	vim.notify("[Plugins] Packer.nvim installed.")
 end
+vim.opt.rtp:prepend(lazy_path)
 
--- protected call to prevent error in the first use
-local packer = require_plugin("packer")
-if packer == nil then
+local lazy = require_plugin("lazy")
+if lazy == nil then
 	return
 end
 
--- Packer.nvim setup
-packer.startup({
-	function(use)
-		-------------------------------------------------------------------------------------------------------------------
-		-- Packer.nvim: the package manager which can manage itself
-		-------------------------------------------------------------------------------------------------------------------
-		use("wbthomason/packer.nvim")
+-- lazy.nvim setup
+lazy.setup({
 
-		-------------------------------------------------------------------------------------------------------------------
-		-- Basic Plugins
-		-------------------------------------------------------------------------------------------------------------------
+	---------------------------------------------------------------------------------------------------------------------
+	-- Basic Plugins
+	---------------------------------------------------------------------------------------------------------------------
 
-		-- nvim-notify: override the default notification persentation
-		use({
-			"rcarriga/nvim-notify",
-			config = function()
-				require("plugins.notify")
-			end,
-		})
-
-		-- colorscheme: catppuccin.nvim
-		use({
-			"catppuccin/nvim",
-			as = "catppuccin",
-			config = function()
-				require("plugins.colorscheme")
-			end,
-		})
-
-		-- lualine: footer
-		use({
-			"nvim-lualine/lualine.nvim",
-			requires = {
-				"nvim-tree/nvim-web-devicons",
-				"arkav/lualine-lsp-progress",
-			},
-			config = function()
-				require("plugins.lualine")
-			end,
-		})
-
-		-- nvim-tree: file explorer
-		use({
-			"nvim-tree/nvim-tree.lua",
-			requires = "nvim-tree/nvim-web-devicons",
-			config = function()
-				require("plugins.tree")
-			end,
-		})
-
-		-- bufferline: header for tabs
-		use({
-			"akinsho/bufferline.nvim",
-			after = "catppuccin",
-			requires = {
-				"nvim-tree/nvim-web-devicons",
-				"moll/vim-bbye",
-			},
-			config = function()
-				require("plugins.bufferline")
-			end,
-		})
-
-		-- nvim-treesitter: code highlighting
-		use({
-			"nvim-treesitter/nvim-treesitter",
-			run = ":TSUpdate",
-			requires = {
-				"nvim-treesitter/nvim-treesitter-refactor",
-				"nvim-treesitter/nvim-treesitter-textobjects",
-			},
-			config = function()
-				require("plugins.treesitter")
-			end,
-		})
-
-		-- indent-blankline: UI enhancement for context identification
-		use({
-			"lukas-reineke/indent-blankline.nvim",
-			config = function()
-				require("plugins.blankline")
-			end,
-		})
-
-		-- nvim-autopairs: automatically generate paired characters
-		use({
-			"windwp/nvim-autopairs",
-			config = function()
-				require("plugins.autopairs")
-			end,
-		})
-
-		-- null-ls: automatic formatting
-		use({
-			"jose-elias-alvarez/null-ls.nvim",
-			requires = "nvim-lua/plenary.nvim",
-			config = function()
-				require("plugins.null-ls")
-			end,
-		})
-
-		-- Comment.nvim: fast (un)commenting
-		use({
-			"numToStr/Comment.nvim",
-			config = function()
-				require("plugins.comment")
-			end,
-		})
-
-		-- gitsigns.nvim: git status display
-		use({
-			"lewis6991/gitsigns.nvim",
-			config = function()
-				require("plugins.gitsigns")
-			end,
-		})
-
-		-- telescope: fuzzy finder over lists
-		use({
-			"nvim-telescope/telescope.nvim",
-			requires = {
-				"nvim-lua/plenary.nvim",
-				"LinArcX/telescope-env.nvim",
-				"nvim-telescope/telescope-ui-select.nvim",
-			},
-			config = function()
-				require("plugins.telescope")
-			end,
-		})
-
-		-- project.nvim: all-in-one project management
-		use({
-			"ahmedkhalf/project.nvim",
-			config = function()
-				require("plugins.project")
-			end,
-		})
-
-		-- dashboard-nvim: dashboard settings
-		use({
-			"glepnir/dashboard-nvim",
-			event = "VimEnter",
-			requires = "nvim-tree/nvim-web-devicons",
-			config = function()
-				require("plugins.dashboard")
-			end,
-		})
-
-		-- aerial.nvim: code outline navigation
-		use({
-			"stevearc/aerial.nvim",
-			config = function()
-				require("plugins.aerial")
-			end,
-		})
-
-		-------------------------------------------------------------------------------------------------------------------
-		-- LSP and Completion Settings
-		-------------------------------------------------------------------------------------------------------------------
-
-		-- completion core engine
-		use({ "hrsh7th/nvim-cmp" })
-
-		-- completion sources
-		use({ "hrsh7th/vim-vsnip" })
-		use({ "hrsh7th/cmp-vsnip" })
-		use({ "hrsh7th/cmp-nvim-lsp" }) -- basic completion config
-		use({ "hrsh7th/cmp-buffer" }) -- completion for buffer
-		use({ "hrsh7th/cmp-path" }) -- completion for path
-		use({ "hrsh7th/cmp-cmdline" }) -- completion for command lines
-		use({ "f3fora/cmp-spell" }) -- spell check
-		use({ "hrsh7th/cmp-nvim-lsp-signature-help" })
-
-		-- Mason LSP engine
-		use({
-			"williamboman/mason.nvim",
-			run = ":MasonUpdate",
-		})
-
-		-- LSP configuration
-		use({ "neovim/nvim-lspconfig" })
-		use({ "williamboman/mason-lspconfig.nvim" })
-
-		-- snippet engine
-		use({ "L3MON4D3/LuaSnip" })
-		use({ "saadparwaiz1/cmp_luasnip" })
-		use({ "rafamadriz/friendly-snippets" })
-
-		-- miscellaneous
-		use({ "onsails/lspkind-nvim" })
-		use({ "folke/neodev.nvim" })
-
-		if packer_bootstrap then
-			packer.sync()
-		end
-	end,
-	config = {
-		max_jobs = 16, -- max jobs for concurrent setup
+	-- nvim-notify: override the default notification presentation
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("plugins.notify")
+		end,
 	},
+
+	-- colorscheme: catppuccin.nvim
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("plugins.colorscheme")
+		end,
+	},
+
+	-- lualine: footer
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"arkav/lualine-lsp-progress",
+		},
+		config = function()
+			require("plugins.lualine")
+		end,
+	},
+
+	-- nvim-tree: file explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("plugins.tree")
+		end,
+	},
+
+	-- bufferline: header for tabs
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = {
+			"catppuccin/nvim",
+			"nvim-tree/nvim-web-devicons",
+			"moll/vim-bbye",
+		},
+		config = function()
+			require("plugins.bufferline")
+		end,
+	},
+
+	-- nvim-treesitter: code highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-refactor",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function()
+			require("plugins.treesitter")
+		end,
+	},
+
+	-- indent-blankline: UI enhancement for context identification
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		dependencies = {
+			"catppuccin/nvim",
+		},
+		config = function()
+			require("plugins.blankline")
+		end,
+	},
+
+	-- nvim-autopairs: automatically generate paired characters
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("plugins.autopairs")
+		end,
+	},
+
+	-- null-ls: automatic formatting
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("plugins.null-ls")
+		end,
+	},
+
+	-- Comment.nvim: fast (un)commenting
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("plugins.comment")
+		end,
+	},
+
+	-- gitsigns.nvim: git status display
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("plugins.gitsigns")
+		end,
+	},
+
+	-- telescope: fuzzy finder over lists
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"LinArcX/telescope-env.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			require("plugins.telescope")
+		end,
+	},
+
+	-- project.nvim: all-in-one project management
+	{
+		"ahmedkhalf/project.nvim",
+		config = function()
+			require("plugins.project")
+		end,
+	},
+
+	-- dashboard-nvim: dashboard settings
+	{
+		"glepnir/dashboard-nvim",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("plugins.dashboard")
+		end,
+	},
+
+	-- aerial.nvim: code outline navigation
+	{
+		"stevearc/aerial.nvim",
+		config = function()
+			require("plugins.aerial")
+		end,
+	},
+
+	-------------------------------------------------------------------------------------------------------------------
+	-- LSP and Completion Settings
+	-------------------------------------------------------------------------------------------------------------------
+
+	-- completion core engine
+	{ "hrsh7th/nvim-cmp" },
+
+	-- completion sources
+	{ "hrsh7th/vim-vsnip" },
+	{ "hrsh7th/cmp-vsnip" },
+	{ "hrsh7th/cmp-nvim-lsp" }, -- basic completion config
+	{ "hrsh7th/cmp-buffer" }, -- completion for buffer
+	{ "hrsh7th/cmp-path" }, -- completion for path
+	{ "hrsh7th/cmp-cmdline" }, -- completion for command lines
+	{ "f3fora/cmp-spell" }, -- spell check
+	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+
+	-- Mason LSP engine
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+	},
+
+	-- LSP configuration
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason-lspconfig.nvim" },
+
+	-- snippet engine
+	{ "L3MON4D3/LuaSnip" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "rafamadriz/friendly-snippets" },
+
+	-- miscellaneous
+	{ "onsails/lspkind-nvim" },
+	{ "folke/neodev.nvim" },
 })
